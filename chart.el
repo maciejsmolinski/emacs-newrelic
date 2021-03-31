b8c1c53187584eae26ed50fb77c483acdb605ea5
Maciej Smolinski <contact@maciejsmolinski.com>
2021-04-01 01:31:43 +0200

    feat: run graphql against real api to collect accounts data

--- chart.el
+++ chart.el
@@ -1,6 +1,8 @@
 ;; -*- lexical-binding: t -*-
 ;; (setf lexical-binding t)
 
+(setq api-key "<API-KEY>")
+
 (defun select-account (account-id)
   "Prints the given ACCOUNT-ID in the current buffer."
   (interactive "nAccount ID: ")
@@ -8,8 +10,6 @@
     (message account-id-as-string)
     (save-excursion
       (insert account-id-as-string))))
-
-;; (call-interactively 'select-account)
 
 (setq accounts '(("First" 1) ("Second" 2)))
 
@@ -30,7 +30,27 @@
     :headers (append '(("Content-Type" . "application/json")) headers)
     :data (json-encode query)
     :parser 'json-read
-    :success (cl-function (lambda (&key data &allow-other-keys) (funcall success-handler data)))))
+    :success (cl-function (lambda (&key data &allow-other-keys) (funcall success-handler data)))
+    :error (cl-function (lambda (&key error-thrown data &allow-other-keys) (message (format "%s %s" error-thrown data))))))
+
+(graphql
+ "https://stormy.maciejsmolinski.com/graphql"
+ '(("query" . "{ sessions { id slug } }"))
+ nil
+ 'on-success)
+
+(defun on-success-handler (data)
+  (let* ((accounts-data (let-alist data .data.actor.accounts))
+         (account-items (seq-map (lambda (a) (list (cdadr a) (cdar a))) accounts-data)))
+    (setq accounts account-items)
+    (call-interactively 'select-account-2)))
+
+(graphql
+ "https://api.newrelic.com/graphql"
+ '(("query" . "{ actor { accounts { id name } } }"))
+ nil
+ 'on-success-handler
+ (list `("Api-Key" . ,api-key)))
 
 (let ((image-path "https://gorgon.nr-assets.net/image/02e53da9-e9d8-4f3b-aa7a-30438111d898?type=line")
       (image nil))
@@ -38,9 +58,3 @@
       (url-retrieve-synchronously image-path)
     (setq image (buffer-substring (1+ url-http-end-of-headers) (point-max))))
   (insert-image (create-image image nil t :height 300)))
-
-(graphql
- "https://stormy.maciejsmolinski.com/graphql"
- '(("query" . "{ sessions { id } }"))
- nil
- 'on-success)
