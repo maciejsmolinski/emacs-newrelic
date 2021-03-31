4a5027c0170063f9b88eb3b9b8d2aa78b2984ac3
Maciej Smolinski <contact@maciejsmolinski.com>
2021-04-01 01:36:13 +0200

    refactor: remove unused code

--- chart.el
+++ chart.el
@@ -2,26 +2,14 @@
 ;; (setf lexical-binding t)
 
 (setq api-key "<API-KEY>")
+(setq accounts '(("No accounts available" 0)))
 
-(defun select-account (account-id)
-  "Prints the given ACCOUNT-ID in the current buffer."
-  (interactive "nAccount ID: ")
-  (let ((account-id-as-string (number-to-string account-id)))
-    (message account-id-as-string)
-    (save-excursion
-      (insert account-id-as-string))))
-
-(setq accounts '(("First" 1) ("Second" 2)))
-
-(defun select-account-2 (account-name)
-  (interactive (list (completing-read "Select account: " accounts nil t)))
+(defun select-account (account-name)
+  (interactive (list (completing-read "Select account: " (seq-map car accounts) nil t)))
   (message "Account %s with id %d" account-name (cadr (assoc account-name accounts))))
 
 (use-package request)
 (use-package url)
-
-(defun on-success (data)
-  (message "I received: %S" data))
 
 (defun graphql (endpoint query variables success-handler &optional headers)
   (request
@@ -33,17 +21,11 @@
     :success (cl-function (lambda (&key data &allow-other-keys) (funcall success-handler data)))
     :error (cl-function (lambda (&key error-thrown data &allow-other-keys) (message (format "%s %s" error-thrown data))))))
 
-(graphql
- "https://stormy.maciejsmolinski.com/graphql"
- '(("query" . "{ sessions { id slug } }"))
- nil
- 'on-success)
-
 (defun on-success-handler (data)
   (let* ((accounts-data (let-alist data .data.actor.accounts))
          (account-items (seq-map (lambda (a) (list (cdadr a) (cdar a))) accounts-data)))
     (setq accounts account-items)
-    (call-interactively 'select-account-2)))
+    (call-interactively 'select-account)))
 
 (graphql
  "https://api.newrelic.com/graphql"
